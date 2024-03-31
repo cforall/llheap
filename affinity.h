6a4bc25e42c4d6f3212c4f296b8ec1149d263575
Peter A. Buhr <pabuhr@uwaterloo.ca>
2024-03-31 16:23:47 -0400

    add machines swift and prolog, remove machine scspc482 from affinity settings

--- affinity.h
+++ affinity.h
@@ -7,16 +7,18 @@ void affinity( unsigned int tid, cpu_set_t & mask ) {
 	tid *= 8; // seperate caches
 	#elif defined( algol )								// ARM
 	enum { OFFSETSOCK = 1 /* 0 origin */, SOCKETS = 2, CORES = 48, HYPER = 1 };
+	#elif defined( prolog )								// ARM
+	enum { OFFSETSOCK = 0 /* 0 origin */, SOCKETS = 1, CORES = 128, HYPER = 1 };
 	#elif defined( nasus )								// AMD
 	enum { OFFSETSOCK = 1 /* 0 origin */, SOCKETS = 2, CORES = 64, HYPER = 1 };
 	#elif defined( jax )								// Intel
 	enum { OFFSETSOCK = 1 /* 0 origin */, SOCKETS = 4, CORES = 24, HYPER = 2 /* wrap on socket */ };
 	#elif defined( pyke )								// Intel
 	enum { OFFSETSOCK = 1 /* 0 origin */, SOCKETS = 2, CORES = 24, HYPER = 2 /* wrap on socket */ };
+	#elif defined( swift )								// Intel
+	enum { OFFSETSOCK = 1 /* 0 origin */, SOCKETS = 2, CORES = 128, HYPER = 2 /* wrap on socket */ };
 	#elif defined( cfapi1 )								// raspberrypi
 	enum { OFFSETSOCK = 0 /* 0 origin */, SOCKETS = 1, CORES = 4, HYPER = 1 };
-	#elif defined( scspc482 )							// Mubeen PC
-	enum { OFFSETSOCK = 0 /* 0 origin */, SOCKETS = 1, CORES = 6, HYPER = 1 };
 	#else // default
 	enum { OFFSETSOCK = 0 /* 0 origin */, SOCKETS = 1, CORES = 8, HYPER = 1 };
 	#endif // HOSTS
