64ce7b0e26c35259b7cda18930376d5e5d976259
Peter A. Buhr <pabuhr@uwaterloo.ca>
2026-01-27 10:46:44 -0500

    update benchmark to accurately find and display crossover point for shorting realloc

--- reallocshort.cc
+++ reallocshort.cc
@@ -6,16 +6,26 @@ static inline void * pass( void * v ) {					// prevent eliding, cheaper than vol
 } // pass
 
 int main() {
-	for ( size_t p = 10; p <= 100; p += 10 ) {
+	// no output => never reuse storage (always realloc)
+	// 99% => always reuse storage (never realloc)
+	// 50% => only realloc when storage size drops below 50% of original
+	bool never = true;
+	for ( size_t p = 0; p <= 100; p += 10 ) {
+		if ( p == 0 ) p = 1;							// 99% shrinkage
 		bool reuse = false;
 		for ( size_t s = 64; s < 16 * 1024; s <<= 1 ) {
 			void * prev = pass( malloc( s ) );
-			void * curr = pass( realloc( prev, s * p / 100 ) );
-			if ( prev == curr ) { printf( "%zd %zd %zd, ", 100 - p, s, s * p / 100 ); reuse = true; }
+			void * curr = pass( realloc( prev, s * p / 100 ) ); // make smaller
+			if ( prev == curr ) { printf( "%zd%% %zd %zd, ", 100 - p, s, s * p / 100 ); reuse = true; never = false; }
 			free( curr );
 		} // for
+		if ( p == 1 ) {
+			p = 0;
+			if ( reuse ) { printf( "\nalways reuse (never realloc)\n" ); break; }
+		} // if
 		if ( reuse ) printf( "\n" );
 	} // for
+	if ( never ) printf( "never reuse (always realloc)\n" );
 //	malloc_stats();
 }
 
