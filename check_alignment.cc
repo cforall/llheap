4b00b7db79278a9152abc15738fc6589bac061bb
Peter A. Buhr <pabuhr@uwaterloo.ca>
2026-02-25 21:58:43 -0500

    make alignment benchmark stronger to verify default alignment for basic types

--- check_alignment.cc
+++ check_alignment.cc
@@ -1,15 +1,21 @@
 #include <iostream>
-#include <cstdint>
+#include <cstdint>										// intptr_t
 using namespace std;
 #include <malloc.h>
 
 template< typename T > void test( const char * tname, T ) {
-	T * tp = (T * )malloc( sizeof( T ) );
-	cout << tname << " size " << sizeof( T ) << ' ' << (intptr_t)(tp) % alignof(max_align_t) << "(" << malloc_usable_size( tp ) << "), ";
+	// Test multiple allocations in case boundary happens to be alignof(max_align_t).
+	for ( size_t i = 0; i < 100; i += 1 ) {
+		T * tp = (T * )malloc( sizeof( T ) );
+		size_t align = (intptr_t)(tp) % alignof(max_align_t);
+		if ( align != 0 ) {
+			cout << tname << ' ' << align << ", ";
+			break;
+		}
+	}
 }	
 
 int main() {
-	cout << "max_align_t " << alignof(max_align_t) << ", ";
 	test( "char", (char)0 );
 	test( "short int", (short int)0 );
 	test( "int", (int)0 );
@@ -20,6 +26,6 @@ int main() {
 	test( "long double", (long double)0 );
 	test( "_Complex", (_Complex)0 );
 	test( "double _Complex", (double _Complex)0 );
-	test( "long double _Complex", (double _Complex)0 );
+	test( "long double _Complex", (long double _Complex)0 );
 	cout << endl;
 }
